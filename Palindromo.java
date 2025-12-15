7cbd02da3c7b8a10dc32f1fdf2d240e99cd16fec
eduardrubioa-prog <eduard.rubioa@gmail.com>
2025-12-15 11:48:55 -0500

    añadir palindromo

---
+++ Palindromo.java
@@ -0,0 +1,16 @@
+public class Palindromo{
+    public static void main(String[] args){
+        String palabra = "reconocr";
+        int ultima_posicion = palabra.length()-1;
+        int primera_posicion = 0;
+        while (primera_posicion < ultima_posicion){
+            if (palabra.charAt(primera_posicion) != palabra.charAt(ultima_posicion)){
+                System.out.println("No es palindromo");
+                return;
+            }
+            primera_posicion++;
+            ultima_posicion--;
+        }
+        System.out.println("Es palidromo");
+    }
+}
